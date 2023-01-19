75828764f018d622a4aaf391c2c0ead2f5a27f74
Dominic Gaona <D.gaona1994@gmail.com>
2023-01-18 21:08:29 -0600

    delete iframe from index

--- Components/Form.js
+++ Components/Form.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import formStyles from "../styles/Form.module.css";
-import ClipLoader from "react-spinners/ClipLoader";
 
 function Form() {
   return <></>;
