d73425d01a8634ef0ce0b6bd8c380cf643997eb6
Dominic Gaona <D.gaona1994@gmail.com>
2023-01-18 20:54:35 -0600

    removed form component from index

--- pages/index.js
+++ pages/index.js
@@ -227,9 +227,7 @@ export default function Home() {
         {/* Contact Me Section */}
         <section id={indexStyles.contactMe}>
           <div className={indexStyles.contactMeContainer}>
-            <div className={indexStyles.contactMeContainerLeft}>
-              <Form />
-            </div>
+            <div className={indexStyles.contactMeContainerLeft}></div>
             <div className={indexStyles.contactMeContainerRight}>
               <GoogleMaps />
             </div>
