49aaf0b05c8c98977ddc8cfbd234aefa94e62a8d
Dominic Gaona <D.gaona1994@gmail.com>
2023-03-23 20:28:32 -0500

    fixed footer

--- Components/Transition.js
+++ Components/Transition.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-// import { animate, AnimatePresence, motion } from "framer-motion";
-// import Projects from "../pages/Projects";
+import { animate, AnimatePresence, motion } from "framer-motion";
+import Projects from "../pages/Projects";
 import style from "../styles/Transition.module.css";
-// import { loadingVariant } from "../data/framer-motion config";
-// import Footer from "./Footer";
+import { loadingVariant } from "../data/framer-motion config";
+import Footer from "./Footer";
 
 function Transition({ children }) {
-  //const { asPath } = useRouter();
+  const { asPath } = useRouter();
 
   return (
     <div className={style.containerTransition} style={{ overflow: "hidden" }}>
       {/*Animate the children component/pages*/}
-      {/* <AnimatePresence mode={"wait"} initial={false}>
+      <AnimatePresence mode={"wait"} initial={false}>
         <motion.main
           className={style.mainContainer}
           variants={loadingVariant}
@@ -23,8 +23,8 @@ function Transition({ children }) {
         >
           {children}
         </motion.main>
-      </AnimatePresence> */}
-      {/* <Footer /> */}
+      </AnimatePresence>
+      <Footer />
     </div>
   );
 }
