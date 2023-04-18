c29ad684e6143d54fea0ef5e1e7dd93048f1c0ee
Dominic Gaona <D.gaona1994@gmail.com>
2023-04-17 19:14:50 -0500

    updated framer motion from x7 to x10

--- Components/Transition.js
+++ Components/Transition.js
@@ -11,7 +11,7 @@ function Transition({ children }) {
   return (
     <div style={{ overflow: "hidden" }}>
       {/*Animate the children component/pages*/}
-      <AnimatePresence mode={"wait"}>
+      <AnimatePresence mode={"wait"} initial={false}>
         <motion.div
           className={style.mainContainer}
           variants={loadingVariant}
