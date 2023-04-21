d7af7b576797241f915424274bb3ad8d3588aeeb
Dominic Gaona <D.gaona1994@gmail.com>
2023-04-21 15:53:24 -0500

    deleted some pages for testing

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import Transition from "../Components/Transition";
 import Navigation from "../Components/Navigation/Navigation";
 import globalStyles from "../styles/globals.css";
 import Footer from "../Components/Footer";
-import { motion } from "framer-motion";
 
 function MyApp({ Component }) {
   return (
--- pages/testing.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "../styles/Home.module.css";
-
-function testing() {
-  return <div className={style.testing}>Testing</div>;
-}
-
-export default testing;
