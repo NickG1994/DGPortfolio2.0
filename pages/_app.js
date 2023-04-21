306234cd71940d240364629a2bce59fb08490816
Dominic Gaona <D.gaona1994@gmail.com>
2023-04-21 18:18:50 -0500

    changed out tranisiton component

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,29 @@ import Transition from "../Components/Transition";
 import Navigation from "../Components/Navigation/Navigation";
 import globalStyles from "../styles/globals.css";
 import Footer from "../Components/Footer";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 function MyApp({ Component }) {
+  const router = useRouter();
   return (
     <div style={{ display: "flex" }}>
       {/* Navigation Component */}
 
       <Navigation />
-      <Transition>
-        <Component />
-      </Transition>
+      <AnimatePresence>
+        <motion.main
+          style={{ overflow: "hidden" }}
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Component />
+        </motion.main>
+      </AnimatePresence>
+
       {/*<Footer />*/}
     </div>
   );
