e676051c8a4359f434d5a8cd46cde3779c573040
Dominic Gaona <D.gaona1994@gmail.com>
2023-04-21 15:30:23 -0500

    turned off css minify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: false,
+  swcMinify: true,
+  compress: false,
 };
 
 module.exports = nextConfig;
