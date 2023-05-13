b1aede6d23d1fe9da9a43506184ec0db06cf889f
Dominic Gaona <D.gaona1994@gmail.com>
2023-05-12 23:30:11 -0500

    added vaarious changes

--- pages/Skills.js
+++ pages/Skills.js
@@ -38,7 +38,7 @@ function Skills() {
         <div className={Skillstyle.right}>
           <Image
             src={require("../public/img/skills/Layer 1.png")}
-            loading="loading"
+            loading="lazy"
             className={Skillstyle.image}
             alt="skill image "
           />
