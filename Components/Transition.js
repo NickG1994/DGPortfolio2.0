633dfdfcf69cc8b8b449ec52f6becc711f6bced5
Dominic Gaona <D.gaona1994@gmail.com>
2023-05-20 14:35:23 -0500

    changes from transition component.

--- Components/Transition.js
+++ Components/Transition.js
@@ -11,22 +11,24 @@ const Transition = ({ children }) => {
   const Router = useRouter();
 
   return (
-    <div style={{ overflow: "hidden", width: "100%" }}>
-      <AnimatePresence mode="popLayout" initial={true}>
-        {/*Animate the children component/pages*/}
-        <motion.div
-          className={style.mainContainer}
-          variants={animateOpacity}
-          initial={animateOpacity.initial}
-          animate={animateOpacity.animate}
-          exit={animateOpacity.exit}
-          transition={animateOpacity.transition}
-          key={Router.route}
-        >
-          {children}
-        </motion.div>
+    <>
+      {" "}
+      <AnimatePresence>
+        <div>
+          {/*Animate the children component/pages*/}
+          <motion.div
+            variants={animateOpacity}
+            initial={animateOpacity.initial}
+            animate={animateOpacity.animate}
+            exit={animateOpacity.exit}
+            transition={animateOpacity.transition}
+            key={Router.route}
+          >
+            {children}
+          </motion.div>
+        </div>
       </AnimatePresence>
-    </div>
+    </>
   );
 };
 
