0d4bf6764e7b47effb9587b4488658d668b030cd
Dominic Gaona <D.gaona1994@gmail.com>
2023-05-20 16:03:06 -0500

    changes from transition component.

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,33 @@ import Transition from "../Components/Transition";
 import Navigation from "../Components/Navigation/Navigation";
 import globalStyles from "../styles/globals.css";
 import Footer from "../Components/Footer";
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
+import style from "../styles/Transition.module.css";
+
+import { animateOpacity } from "../data/framer-motion config";
 
 function MyApp({ Component }) {
+  const Router = useRouter();
   return (
     <div style={{ display: "flex" }}>
       {/* Navigation Component */}
 
       <Navigation />
-      <Transition>
-        <Component />
-        <Footer />
-      </Transition>
+      <AnimatePresence mode={"popLayout"} initial="true">
+        {/*Animate the children component/pages*/}
+        <motion.div
+          className={style.mainContainer}
+          variants={animateOpacity}
+          initial={animateOpacity.initial}
+          animate={animateOpacity.animate}
+          exit={animateOpacity.exit}
+          transition={animateOpacity.transition}
+          key={Router.route}
+        >
+          <Component />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
