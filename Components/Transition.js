d9d9bd892de6853bcbc45634978fead02114794b
Dominic Gaona <d.gaona1994@gmail.com>
2023-06-06 19:50:24 -0500

    changed layout for transition back

--- Components/Transition.js
+++ Components/Transition.js
@@ -12,27 +12,24 @@ const Transition = ({ children }) => {
   const Router = useRouter();
 
   return (
-    <>
+    <div className={style.flex} style={{display:"flex"}}>
     {/* Navigation Component */}
     <Navigation />
-      <AnimatePresence mode='wait' initial={"true"} onExitComplete={() => {window.scrollTo(0,0);}}> 
-      <div className={style.flex} style={{display:"flex"}}>
-        <motion.div 
-          style={{width: '100%', position:"relative" }}     
-          layout 
-          variants={animateOpacity}
-          exit={animateOpacity.exit}
-          transition={animateOpacity.transition}     
-          key={Router.route}
-        >
-          {/* Animate the children component/pages */}
-          {children}
-          <Footer />
-        </motion.div>
-        </div>
-      </AnimatePresence>
-    </>
-
+    <AnimatePresence mode='wait' initial={"true"} onExitComplete={() => {window.scrollTo(0,0);}}> 
+      <motion.div 
+        style={{width: '100%', position:"relative" }}     
+        layout 
+        variants={animateOpacity}
+        exit={animateOpacity.exit}
+        transition={animateOpacity.transition}     
+        key={Router.route}
+      >
+        {/* Animate the children component/pages */}
+        {children}
+        <Footer />
+      </motion.div>
+    </AnimatePresence>
+    </div>
   );
 };
 
