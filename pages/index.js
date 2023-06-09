90945cb09ccf85c6557900f54bfd6e1b5dd18856
Dominic Gaona <d.gaona1994@gmail.com>
2023-06-09 14:37:28 -0500

    testing index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import styles
-import styles from "/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 //import next components
 import Head from "next/head";
 import Image from "next/image";
